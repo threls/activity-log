cd93ee18271d6708e1d66653e5988d7bf039d1cc
sabina <sabinahaloci@gmail.com>
2025-03-13 15:29:56 +0100

    remove stubs

--- database/migrations/2025_03_13_142844_update_activity_log.php
+++ database/migrations/2025_03_13_142844_update_activity_log.php
@@ -4,13 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up()
+return new class extends Migration {
+    public function up(): void
     {
         Schema::table('activity_log', function (Blueprint $table) {
             $table->bigInteger('model_id')->unsigned()->nullable()->after('user_id');
             $table->bigInteger('model_type')->unsigned()->nullable()->after('model_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('activity_log', function (Blueprint $table) {
+            $table->dropColumn('model_id');
+            $table->dropColumn('model_type');
         });
     }
 };
